f2cc841948448ed5176c6ca41fd751398481e2f6
musaa99 <musaopaluwa@gmail.com>
2022-03-18 02:14:08 +0100

    added functionality

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,44 +1,60 @@
-import React from 'react'
-import CalculatorTitle from './calculatorTitle'
-import OutputScreen from './outputScreen'
-import Button from './button'
-
+import React, {useState} from 'react'
+import './cal.css';
+//import '../App.css'
 const Calculator = () => {
+
+  const  [result, setResult] = useState("")
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name) )
+  }  
+
+  const clear = (e) => {
+    setResult('');
+  }   
+
+  const backspace = (e) => {
+    setResult(result.slice(0, -1) )
+  }  
+  
+  const calculate = (e) => {
+    try{
+      setResult(eval(result).toString());
+    } catch(err){
+      setResult('Error')
+    }
+   
+  }  
   return (
-    <div className="frame">
-    <CalculatorTitle value="musa Calculator"/>
-    <div class="mainCalc">
-    <OutputScreen/>
-    <div className="button-row">
-      <Button label={'Clear'}/>
-      <Button label={'Delete'}/>
-      <Button label={'.'}/>
-      <Button label={'/'}/>
-    </div>
-    <div className="button-row">
-      <Button label={'7'}/>
-      <Button label={'8'}/>
-      <Button label={'9'}/>
-      <Button label={'*'}/>
-    </div>
-    <div className="button-row">
-      <Button label={'4'}/>
-      <Button label={'5'}/>
-      <Button label={'6'}/>
-      <Button label={'-'}/>
-    </div>
-    <div className="button-row">
-      <Button label={'1'}/>
-      <Button label={'2'}/>
-      <Button label={'3'}/>
-      <Button label={'+'}/>
-    </div>
-    <div className="button-row">
-      <Button label={'0'}/>
-      <Button label={'='}/>
-    </div>
-    </div>
-    </div>
+    <React.Fragment>
+      <div className='container'>
+        <form >
+          <input type="text" value={result}/>
+        </form>
+          <div className='keypad'>
+            <button onClick={clear} id='clear'>  clear</button>
+            <button onClick={backspace} id='backspace'>C</button>
+            <button name='/'onClick={handleClick} >&divide;</button> 
+            <button name='7' onClick={handleClick}>7</button>
+            <button name='8'onClick={handleClick}>8</button>
+            <button name='9'onClick={handleClick}>9</button>
+            <button name='*'onClick={handleClick}>&times;</button>
+            <button name='4'onClick={handleClick}>4</button>
+            <button name='5'onClick={handleClick}>5</button>
+            <button name='6'onClick={handleClick}>6</button>
+            <button name='-'onClick={handleClick}>-</button>
+            <button name='1'onClick={handleClick}>1</button>
+            <button name='2'onClick={handleClick}>2</button>
+            <button name='3'onClick={handleClick}>3</button>
+            <button name='+'onClick={handleClick}>+</button>
+            <button name='0'onClick={handleClick}>0</button>
+            <button name='.'onClick={handleClick}>.</button>
+            <button onClick={calculate} id='result'>=</button>
+
+          </div>
+      </div>
+
+    </React.Fragment>
   )
 }
 
